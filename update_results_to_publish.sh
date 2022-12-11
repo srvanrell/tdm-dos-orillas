103fc6e6b4a3d122a5e2d4e46da6efe5b62316ab
sebastian <srvanrell@gmail.com>
2022-12-11 17:23:55 -0300

    enhancement: make year configurable in results update

--- update_results_to_publish.sh
+++ update_results_to_publish.sh
@@ -1,14 +1,17 @@
+#!/usr/bin/env zsh
+#
 # TODO run rtt publish to get latest version of publishing files
-# Use zsh to run this script
+
+YEAR=${1:-2022}
 
 # Commands to bring latest results from rtt execution
-rsync -avz ../ranking-table-tennis/data_rtt/S2022T* ./snippets/2022/
+rsync -avz ../ranking-table-tennis/data_rtt/S${YEAR}T* ./snippets/${YEAR}/
 
 # Move statistics to images corresponding folder
-folders=(`ls -d1X snippets/2022/S2022T*/`)
+folders=(`ls -d1X snippets/${YEAR}/S${YEAR}T*/`)
 for folder in "${folders[@]}"
 do
     echo "'$folder'"
-    mv -v "${folder}Estadisticas_Acumulado.png" "docs/assets/images/2022/Estadisticas_Acumulado.png"
-    mv -v "${folder}Estadisticas_Por_Torneo.png" "docs/assets/images/2022/Estadisticas_Por_Torneo.png"
+    mv -v "${folder}Estadisticas_Acumulado.png" "docs/assets/images/${YEAR}/Estadisticas_Acumulado.png"
+    mv -v "${folder}Estadisticas_Por_Torneo.png" "docs/assets/images/${YEAR}/Estadisticas_Por_Torneo.png"
 done
